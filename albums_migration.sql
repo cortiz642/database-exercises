d937aa1628105dff953920de9fd525e898540f1b
cortiz642 <chris.o.ortiz93@gmail.com>
2022-06-07 13:44:21 -0500

    albums table created and verified with content

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,10 +1,11 @@
 USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED AUTO_INCREMENT,
     artist VARCHAR(50) NOT NULL,
     name VARCHAR(25) NOT NULL,
     release_date YEAR(4) NOT NULL,
-    sales INT(M) NOT NULL,
+    sales FLOAT,
     genre VARCHAR(25) NOT NULL,
     PRIMARY KEY (id)
 );
