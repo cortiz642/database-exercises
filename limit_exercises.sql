54432eb6e679780ea3813195206d001e8e5683fa
cortiz642 <chris.o.ortiz93@gmail.com>
2022-06-09 10:22:55 -0500

    ordered by employee number with top five salaries displaying only employee number with a limit of 5

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,5 +1,5 @@
 USE employees;
-
+USE salaries;
 
 SELECT DISTINCT title FROM titles;
 
@@ -7,3 +7,6 @@ SELECT last_name
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya') order by last_name desc limit 10;
 
+SELECT emp_no
+FROM salaries
+ORDER BY salary desc limit 5;
