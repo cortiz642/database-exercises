ad650cf4a4061c00327f12e245febd9662b7ac09
cortiz642 <chris.o.ortiz93@gmail.com>
2022-06-09 07:36:47 -0500

    duplicated previous query and updated to find all employees whose last name starts and end with E

--- where_exercises.sql
+++ where_exercises.sql
@@ -7,3 +7,14 @@ WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 SELECT last_name
 FROM employees
 WHERE last_name LIKE '%q%';
+
+SELECT * FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE ('%E') AND last_name LIKE ('E%');
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
