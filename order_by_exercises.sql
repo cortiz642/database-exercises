19a534bbcd3a8bf08ef60e98aeb80124398dab87
cortiz642 <chris.o.ortiz93@gmail.com>
2022-06-10 12:59:18 -0500

    slight revise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -20,3 +20,16 @@ SELECT emp_no, last_name
 FROM employees
 WHERE last_name LIKE ('%E') AND last_name LIKE ('E%') ORDER BY emp_no desc;
 
+# SELECT DISTINCT first_name
+# FROM employees
+# WHERE first_name NOT LIKE '%a%';
+
+# SELECT *
+# FROM employees
+# WHERE year(birth_date) BETWEEN 1950 AND 1959
+#   AND month(birth_date) = 7
+#   AND day(birth_date) = 4;
+
+# SELECT NOW();
+#
+# SELECT CURDATE();
