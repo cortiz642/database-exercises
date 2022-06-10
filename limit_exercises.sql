1280a4be07a4ee1830cbdef2f3f5feac6b327c41
cortiz642 <chris.o.ortiz93@gmail.com>
2022-06-10 12:58:41 -0500

    slight revise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -10,3 +10,7 @@ WHERE first_name IN ('Irena', 'Vidya', 'Maya') order by last_name desc limit 10;
 SELECT emp_no
 FROM salaries
 ORDER BY salary desc limit 5;
+
+SELECT emp_no
+FROM salaries
+ORDER BY emp_no limit 100 offset 15;
